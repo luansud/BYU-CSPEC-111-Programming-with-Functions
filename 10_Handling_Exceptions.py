7729f592cc04f62a3df443629ff51016a470c3e0
luansud <luanfmedeiros@hotmail.coM>
2023-06-23 11:20:58 -0300

    Finishing week 10

---
+++ 10_Handling_Exceptions.py
@@ -0,0 +1,9 @@
+#                       TYPES OS ERRORS
+
+#  TypeError = datatype error (Ex:index or a number, or round a str )
+#  ValueError = Value error (Ex: Try to convert a str to a number)
+#  ZeroDivisionError = Trying divide a number by Zero Ex: 6/0
+#  IndexError = Trying find a index that it's not in the list, Ex: list[amor, glayce] list[5]  
+#  KeyError = Trying to find a key in a dictionary that doesn't exist
+#  FileNotFoundError = Looking for a File that doesn't exist in the directory
+#  PermissionError = When the user doesn't have permission for access the file
--- receipt.py
+++ receipt.py
@@ -1,18 +1,52 @@
+from datetime import datetime
+
 def main():
-    products_dict = read_dictionary("products.csv")
-    print(f"All Products \n{products_dict}")
-    print("\nRequested Items")
-    price = 0
-    with open("request.csv") as request:
-        next(request)
-        for line in request:
-            line = line.strip()
-            cod, quantity = line.split(",")
-            full_product = products_dict[cod]
-            price = float(full_product[2])
-            quantity = int(quantity)
-            print(f"{full_product[1]}: {quantity} ${price:.2f}")
-                
+    try:
+        products_dict = read_dictionary("products.csv",0)
+        print("\nInkom Emporium")
+        price = 0
+        subtotal = 0
+        total = 0
+        items = 0
+        tax = 0
+        with open("request.csv") as request:
+            next(request)
+            for line in request:
+                line = line.strip()
+                cod, quantity = line.split(",")
+                if cod in products_dict.keys():
+                    full_product = products_dict[cod]
+                    price = float(full_product[2])
+                    quantity = int(quantity)
+                    print(f"{full_product[1]}: {quantity} ${price:.2f}")
+                    items += 1
+                    subtotal += price*quantity
+                 
+                            
+        tax = subtotal*0.06
+        total = subtotal+tax
+        current_date = datetime.now()
+        day = datetime.today().strftime('%A')
+        wed_discount = total-(total*0.10)
+        print(f"\nNumber of Items: {items}")
+        print(f"Subtotal: ${subtotal:.2f}")
+        print(f"Sales Tax: ${tax:.2f}")
+        if day == "Friday":
+            print(f"Wednesday Discount: -${(total*0.10):.2f}")
+            print(f"Total: ${wed_discount:.2f}")
+        else:
+            print(f"Total: ${total:.2f}")
+        print(f"\nThank you for shopping at the Inkom Emporium.")
+        print(f"{current_date:%A %B %d %I:%M:%S %p %Y}")
+    
+    
+    except FileNotFoundError as file_error:
+        print(file_error)
+    except PermissionError as perm_error:
+        print(perm_error)
+    except KeyError as key_error:
+        print(key_error)
+     
 
 def read_dictionary(filename,key_column_index):
     """Read the contents of a CSV file into a compound
